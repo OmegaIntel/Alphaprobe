0de3fb6a7c01cf612a34a37ce6bd85143332388e
Yakov Keselman <yk@helvetiaeducation.ch>
2024-09-10 11:46:42 -0500

    Tweaks

--- notebooks/aws_summary_template.py
+++ notebooks/aws_summary_template.py
@@ -1,163 +1,161 @@
 from copy import deepcopy
 
 
-_PARTS = [
-    {
-        "key_statistics": {
-            "type": "object",
-            "description": "Key Statistics",
-            "properties": {
-                "revenue": {
-                    "type": "string",
-                    "description": "The annual revenue."
-                },
-                "historical_revenue_growth": {
-                    "type": "string",
-                    "description": "Historical revenue growth."
-                },
-                "projected_revenue_growth": {
-                    "type": "string",
-                    "description": "Projected revenue growth."
-                },
-                "profit margins": {
-                    "type": "string",
-                    "description": "Profit margins."
-                },
-            }
-        },
-        "executive_summary": {
-            "type": "string",
-            "description": "Executive summary."
-        },
-        "current_performance": {
-            "type": "string",
-            "description": "Current Performance."
-        },
-        "future_outlook": {
-            "type": "string",
-            "description": "Future Outlook."
-        },
-        "industry_definition": {
-            "type": "string",
-            "description": "Industry Definition and Impact."
-        },
-        "swot_analysis": {
-            "type": "object",
-            "description": "SWOT Analysis",
-            "properties": {
-                "strengths": {
-                    "type": "string",
-                    "description": "Strengths."
-                },
-                "weaknesses": {
-                    "type": "string",
-                    "description": "Weaknesses."
-                },
-                "opportunities": {
-                    "type": "string",
-                    "description": "Opportunities."
-                },
-                "threats": {
-                    "type": "string",
-                    "description": "Threats."
-                },
-            }
-        },
-    },
-
-    {
-        "supply_chain": {
-            "type": "object",
-            "description": "Supply Chain Related Industries",
-            "properties": {
-                "tier_1_suppliers": {
-                    "name": "Tier 1 suppliers",
-                    "type": "array",
-                    "items": { "type": "string" },
-                    "description": "Tier 1 suppliers: 2-3 industry names."
-                },
-                "tier_2_suppliers": {
-                    "name": "Tier 2 suppliers",
-                    "type": "array",
-                    "items": { "type": "string" },
-                    "description": "Tier 2 suppliers: 2-3 industry names."
-                },
-                "tier_1_buyers": {
-                    "name": "Tier 1 buyers",
-                    "type": "array",
-                    "items": { "type": "string" },
-                    "description": "Tier 2 buyers: 4-5 industry names."
-                },
-            }
-        },
-        "products_and_services": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "product_or_service": {
-                        "type": "string",
-                        "description": "The name of product or service."
-                    },
-                    "percentage": {
-                        "type": "number",
-                        "description": "The percentage of the product or service.",
-                        "minimum": 1,
-                        "maximum": 99,
-                        "multipleOf": 0.1,
-                    }
-                }
+_PARTS = [{
+    "key_statistics": {
+        "type": "object",
+        "description": "Key Statistics",
+        "properties": {
+            "revenue": {
+                "type": "string",
+                "description": "The annual revenue."
             },
-            "description": "Products & Services: names and percentages of contribution of product/service to industry."
-        },
-        "key_trends": {
-            "type": "array",
-            "items": { "type": "string" },
-            "description": "Key Trends."
-        },
-        "market_segmentation": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "segment": {
-                        "type": "string",
-                        "description": "The market segment."
-                    },
-                    "percentage": {
-                        "type": "number",
-                        "description": "The percentage of the market segment.",
-                        "minimum": 1,
-                        "maximum": 99,
-                        "multipleOf": 0.1,
-                    }
-                }
+            "historical_revenue_growth": {
+                "type": "string",
+                "description": "Historical revenue growth."
             },
-            "description": "Market Segmentation: customer segmentation by sector – names and percentages."
-        },
+            "projected_revenue_growth": {
+                "type": "string",
+                "description": "Projected revenue growth."
+            },
+            "profit margins": {
+                "type": "string",
+                "description": "Profit margins."
+            },
+        }
     },
+    "executive_summary": {
+        "type": "string",
+        "description": "Executive summary."
+    },
+    "current_performance": {
+        "type": "string",
+        "description": "Current Performance."
+    },
+    "future_outlook": {
+        "type": "string",
+        "description": "Future Outlook."
+    },
+    "industry_definition": {
+        "type": "string",
+        "description": "Industry Definition and Impact."
+    },
+    "swot_analysis": {
+        "type": "object",
+        "description": "SWOT Analysis",
+        "properties": {
+            "strengths": {
+                "type": "string",
+                "description": "Strengths."
+            },
+            "weaknesses": {
+                "type": "string",
+                "description": "Weaknesses."
+            },
+            "opportunities": {
+                "type": "string",
+                "description": "Opportunities."
+            },
+            "threats": {
+                "type": "string",
+                "description": "Threats."
+            },
+        }
+    },
+},
 
-    {
-        "related_international_industries": {
-            "type": "array",
-            "items": { "type": "string" },
-            "description": "Related International Industries."
+{
+    "supply_chain": {
+        "type": "object",
+        "description": "Supply Chain Related Industries",
+        "properties": {
+            "tier_1_suppliers": {
+                "name": "Tier 1 suppliers",
+                "type": "array",
+                "items": { "type": "string" },
+                "description": "Tier 1 suppliers: 2-3 industry names."
+            },
+            "tier_2_suppliers": {
+                "name": "Tier 2 suppliers",
+                "type": "array",
+                "items": { "type": "string" },
+                "description": "Tier 2 suppliers: 2-3 industry names."
+            },
+            "tier_1_buyers": {
+                "name": "Tier 1 buyers",
+                "type": "array",
+                "items": { "type": "string" },
+                "description": "Tier 2 buyers: 4-5 industry names."
+            },
+        }
+    },
+    "products_and_services": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "product_or_service": {
+                    "type": "string",
+                    "description": "The name of product or service."
+                },
+                "percentage": {
+                    "type": "number",
+                    "description": "The percentage of the product or service.",
+                    "minimum": 1,
+                    "maximum": 99,
+                    "multipleOf": 0.1,
+                }
+            }
         },
-        "competitive_landscape": {
-            "type": "string",
-            "description": "Competitive Landscape."
+        "description": "Products & Services: names and percentages of contribution of product/service to industry."
+    },
+    "key_trends": {
+        "type": "array",
+        "items": { "type": "string" },
+        "description": "Key Trends."
+    },
+    "market_segmentation": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "segment": {
+                    "type": "string",
+                    "description": "The market segment."
+                },
+                "percentage": {
+                    "type": "number",
+                    "description": "The percentage of the market segment.",
+                    "minimum": 1,
+                    "maximum": 99,
+                    "multipleOf": 0.1,
+                }
+            }
         },
-        "costs_and_operations": {
-            "type": "string",
-            "description": "Costs & Operations."
-        },
-        "major_players": {
-            "type": "array",
-            "items": { "type": "string" },
-            "description": "Major Players."
-        },
-    }
-]
+        "description": "Market Segmentation: customer segmentation by sector – names and percentages."
+    },
+},
+
+{
+    "related_international_industries": {
+        "type": "array",
+        "items": { "type": "string" },
+        "description": "Related International Industries."
+    },
+    "competitive_landscape": {
+        "type": "string",
+        "description": "Competitive Landscape."
+    },
+    "costs_and_operations": {
+        "type": "string",
+        "description": "Costs & Operations."
+    },
+    "major_players": {
+        "type": "array",
+        "items": { "type": "string" },
+        "description": "Major Players."
+    },
+}]
 
 
 _ROOT = {
