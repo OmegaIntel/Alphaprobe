125bf13140803c48273d492cae128f2571a9fb47
Yakov Keselman <yk@helvetiaeducation.ch>
2024-09-19 16:29:05 -0500

    CHeckpoint

---
+++ notebooks/Templates/aws_basic_info_template.py
@@ -0,0 +1,16 @@
+"""Basic info template"""
+
+TEMPLATE = [{
+    "title": {
+        "type": "string",
+        "description": "Document title."
+    },
+    "source": {
+        "type": "string",
+        "description": "The organization that authored the document."
+    },
+    "last_updated": {
+        "type": "string",
+        "description": "Date when the document was last updated."
+    },
+}]
