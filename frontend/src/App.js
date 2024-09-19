d6f66bc7387b130371ce6011d1001938ed7bbf31
Tthra <>
2024-09-19 11:37:38 +0530

    Completed UI development of register and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,95 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Chat from './components/Chat';
-import Register from './components/Register';
-import Login from './components/Login';
-import Sidebar from './components/Sidebar';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Chat from "./components/Chat";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token') || '');
-  const [updateSidebarSessions, setUpdateSidebarSessions] = useState(() => () => {});
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+  const [updateSidebarSessions, setUpdateSidebarSessions] = useState(
+    () => () => {}
+  );
 
   const handleSetToken = (newToken) => {
     setToken(newToken);
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
   };
 
   const isLoggedIn = Boolean(token);
+  const ProtectedRoute = ({ isLoggedIn, children }) => {
+    return isLoggedIn ? children : <Navigate to="/login" />;
+  };
 
   return (
     <Router>
       <div className="App">
         {isLoggedIn && (
-          <Sidebar setToken={handleSetToken} setUpdateSidebarSessions={setUpdateSidebarSessions} />
+          <Sidebar
+            setToken={handleSetToken}
+            setUpdateSidebarSessions={setUpdateSidebarSessions}
+          />
         )}
-        <div className={`main-content ${isLoggedIn ? 'with-sidebar' : 'without-sidebar'}`}>
+        <div
+          className={`main-content ${
+            isLoggedIn ? "with-sidebar" : "without-sidebar"
+          }`}
+        >
           <Routes>
-            <Route path="/register" element={isLoggedIn ? <Navigate to="/chat" /> : <Register />} />
-            <Route path="/login" element={isLoggedIn ? <Navigate to="/chat" /> : <Login setToken={handleSetToken} />} />
-            <Route path="/chat" element={<Chat setToken={handleSetToken} updateSidebarSessions={updateSidebarSessions} />} />
-            <Route path="/chat/:chatId" element={<Chat setToken={handleSetToken} updateSidebarSessions={updateSidebarSessions} />} />
-            <Route path="/" element={!isLoggedIn ? <Navigate to="/login" /> : <Navigate to="/chat" />} />
+            <Route
+              path="/register"
+              element={isLoggedIn ? <Navigate to="/chat" /> : <Register />}
+            />
+            <Route
+              path="/login"
+              element={
+                isLoggedIn ? (
+                  <Navigate to="/chat" />
+                ) : (
+                  <Login setToken={handleSetToken} />
+                )
+              }
+            />
+            <Route
+              path="/chat"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}>
+                  <Chat
+                    setToken={handleSetToken}
+                    updateSidebarSessions={updateSidebarSessions}
+                  />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/chat/:chatId"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}>
+                  <Chat
+                    setToken={handleSetToken}
+                    updateSidebarSessions={updateSidebarSessions}
+                  />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/"
+              element={
+                isLoggedIn ? <Navigate to="/chat" /> : <Navigate to="/login" />
+              }
+            />
+
+            <Route
+              path="*"
+              element={<Navigate to={isLoggedIn ? "/chat" : "/login"} />}
+            />
           </Routes>
         </div>
       </div>
---
+++ frontend/src/components/Login/index.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+const Login = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(""); // Clear any previous errors
+
+    const formData = new FormData();
+    formData.append("username", email);
+    formData.append("password", password);
+
+    try {
+      const response = await axios.post(`${API_BASE_URL}/token`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (response.status === 200) {
+        setToken(response.data.access_token);
+        navigate("/chat");
+      }
+    } catch (error) {
+      setError("Login failed. Please check your credentials.");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen relative">
+      <div className="auth-gradient w-[60vw] h-full absolute bottom-[20vw] right-[60vw]"></div>
+      <img
+        src="/images/company-logo.png"
+        className="absolute top-0 right-0"
+        alt="rahil"
+      />
+      <div className="p-8 rounded-lg w-[400px]">
+        <div className="mb-10">
+          <h2 className="text-[20px] text-white tracking-widest font-bold">
+            LOGIN
+          </h2>
+          <p className="text-sm text-white">
+            Don’t have an account?{" "}
+            <Link to="/register" className="text-[#33bbff]">
+              Register
+            </Link>
+          </p>
+        </div>
+        <form>
+          <div className="mb-5 flex flex-col gap-3">
+            <label className="text-xs text-[#8a8a90]">Email</label>
+            <input
+              type="email"
+              placeholder="example@omegaintelligence.org"
+              className="w-full p-[10px] bg-[#212126] border border-[#303038] rounded text-white text-sm outline-none placeholder:text-[#5c5c5c]"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-5 flex flex-col gap-3 relative">
+            <label className="text-xs text-[#8a8a90]">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="w-full p-[10px] bg-[#212126] border border-[#303038] rounded text-white text-sm outline-none placeholder:text-[#5c5c5c]"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <span
+              className="absolute right-[15px] top-[40px] text-gray-500 cursor-pointer hover:text-[#33bbff]"
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+            >
+              {showPassword ? (
+                <i className="fa fa-eye-slash"></i>
+              ) : (
+                <i className="fa fa-eye"></i>
+              )}
+            </span>
+          </div>
+          {/* <div className="remember-forgot">
+            <label>
+              Remember me
+              <input type="checkbox" />
+              <span class="checkmark"></span>
+            </label>
+            <a
+              href="https://pro.openbb.co/forgot-password"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Forgot password?
+            </a>
+          </div> */}
+          <div className="flex justify-center mt-4">
+            <button
+              type="submit"
+              className="w-[100px] h-[40px] bg-[#0088cc] rounded text-white text-xs hover:bg-[#0056b3]"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
