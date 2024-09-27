b5e8a1331c9fdb201f968f7001f2f72b4cd261c1
Tthra <>
2024-09-27 15:35:57 +0530

    bugs fixes in login

--- frontend/src/components/dilligence_list/container.jsx
+++ frontend/src/components/dilligence_list/container.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import KanbanBoard from ".";
+import { useModal } from "../UploadFilesModal/ModalContext";
 
 const DilligenceContainer = () => {
+    const { deals, dealId } = useModal();
+    const [currentDeal, setCurrentDeal] = useState("");
+    useEffect(()=>{
+        if(deals && dealId){
+            setCurrentDeal(deals.find((data)=>data.id===dealId));
+        }
+    }, [dealId, deals])
     return (
         <>
-                <div className="bg-[#151518] flex flex-col w-full flex-grow overflow-auto border-l-4 border-[#1e1e1e]">
+            <div className="bg-[#151518] flex flex-col w-full flex-grow overflow-auto border-l-4 border-[#1e1e1e]">
                 <div className="text-4xl font-bold m-10">
-                    Project Alpha
+                    {currentDeal.name}
                 </div>
                 <KanbanBoard />
             </div>
