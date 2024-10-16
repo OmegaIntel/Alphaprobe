52ece47c3a2cb95ca6b6c88470936d71cff0c598
Yakov Keselman <yk@helvetiaeducation.ch>
2024-10-16 12:33:15 -0700

    Checkpoint

---
+++ api/api/api_related_industries.py
@@ -0,0 +1,52 @@
+"""Returns lists of related industries in response to UI requests."""
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import List, Dict
+
+class UserInputForThesis(BaseModel):
+    __root__: Dict[str, str]
+
+related_industries_router = APIRouter()
+
+"""
+@related_industries_router.post("/api/industries-for-thesis", response_model=None)
+async def industries_for_thesis(request: List[UserInputForThesis]) -> List[str]:
+
+
+from fastapi import APIRouter, HTTPException, Form, Depends, Request
+from pydantic import BaseModel, EmailStr
+from db_models.session import get_db
+from sqlalchemy.orm import Session
+from db_models.demo_requests import DemoRequests
+import uuid
+from typing import Optional
+from sqlalchemy.exc import SQLAlchemyError
+
+demo_request_router = APIRouter()
+
+
+# Pydantic model
+class DemoRequestCreate(BaseModel):
+    name: str
+    company: Optional[str] = None
+    email: EmailStr
+    message: Optional[str] = None
+
+@demo_request_router.post("/api/request-demo", response_model=None)
+async def request_demo(request: DemoRequestCreate, db: Session = Depends(get_db)):
+    try:
+        demo_request = DemoRequests(
+            id=uuid.uuid4(),
+            name=request.name,
+            company=request.company,
+            email=request.email,
+            message=request.message
+        )
+        db.add(demo_request)
+        db.commit()
+        return {"message": "Demo request received successfully"}
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Database error: " + str(e))
+"""
