b85afa687482ef1e045c3c07cc15d049f51b1368
Yakov Keselman <yk@helvetiaeducation.ch>
2024-10-16 13:28:13 -0700

    Add test

--- api/doc_parser/pdf_utils_test.py
+++ api/doc_parser/pdf_utils_test.py
@@ -5,9 +5,9 @@ import os
 
 def test_extract_pages():
     filename = 'test-data/study_id66974_in-depth-report-industry-40.pdf'
-    with extract_pages(filename, first_page=5, last_page=15) as filename:
-        assert os.path.exists(filename)
-    assert not os.path.exists(filename)
+    with extract_pages(filename, first_page=5, last_page=15) as temp_filename:
+        assert os.path.exists(temp_filename)
+    assert not os.path.exists(temp_filename)
 
 
 def test_number_of_pages():
