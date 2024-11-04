a29f304bc8efd30d3deacd0ee424e10bd3026d2a
Yakov Keselman <yk@helvetiaeducation.ch>
2024-11-04 15:49:03 -0800

    Initial tests

---
+++ Omega_scraper/web_scraper.py
@@ -0,0 +1,21 @@
+import urllib.parse
+from playwright.async_api import async_playwright
+
+
+async def find_url(company_name, site):
+    term = urllib.parse.quote_plus(f"{company_name} site:{site}")
+    url = f"https://www.google.com/search?q={term}"
+
+    async with async_playwright() as pw:
+        browser = await pw.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto(url, wait_until="domcontentloaded")
+
+        try:
+            # Retrieve the first result link for the specified site
+            link = await page.locator("a:has(h3)").first.evaluate('el => el.href')
+            return link
+        except Exception as e:
+            print(f"{site} URL not found for {company_name}: {e}")
+        
+        await browser.close()
---
+++ Omega_scraper/web_scraper_test.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from web_scraper import find_url
+
+
+class TestFindURL:
+
+    company_name = "10X Engineered Materials"
+
+    def runtest(self, site: str, correct: str):
+        result = asyncio.run(find_url(self.company_name, site))
+        assert correct == result
+
+    def test_owler(self):
+        self.runtest('owler.com', 'https://www.owler.com/company/10xem')
+
+    def test_crunchbase(self):
+        self.runtest('crunchbase.com', 'https://www.crunchbase.com/organization/10x-engineered-materials')
+
+    def test_pitchbook(self):
+        self.runtest('pitchbook.com', 'https://pitchbook.com/profiles/company/264531-70')
+
+    def test_linkedin(self):
+        self.runtest('linkedin.com', 'https://www.linkedin.com/company/10xem')
+
+    def test_open_corporates(self):
+        self.runtest('opencorporates.com', 'https://sources.opencorporates.com/bots/dk_xbrl_parser/e/c/7/51696536_amNsb3VkczovLzAzL2ZmLzllLzNlL2JjLzkwMjMtNGFkYi04MDA5LTJhZmM2ODNiZDhkZg.pdf')
+
