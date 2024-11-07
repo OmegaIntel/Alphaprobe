a05e455e7eb1b539de7371f1a2a3dd2723b5ab1b
Ayush Tenguria <46924317+ayushtenguria@users.noreply.github.com>
2024-11-07 19:30:47 +0530

    minor fix

--- frontend/src/components/MarketResearch/IndustryReportSection/IndustryReport.jsx
+++ frontend/src/components/MarketResearch/IndustryReportSection/IndustryReport.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import ReportDropdown from "./ReportDropdown"; // Adjust the import path if necessary
-import DownloadReport from "./DownloadReport";
+
 
 const YourComponent = () => {
   const industryState = useSelector((state) => state);
